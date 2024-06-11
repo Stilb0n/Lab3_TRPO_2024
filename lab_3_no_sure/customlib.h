4fed49727cb38c1d06141b475717ef108febf2b0
Stilb0n <stilbon01@gmail.com>
2024-06-11 21:22:43 +0700

    Создана и протестирована  функция getSizeOfFilesIn

--- lab_3_no_sure/customlib.h
+++ lab_3_no_sure/customlib.h
@@ -4,6 +4,23 @@
 #include <QDir>
 #include <QFileInfo>
 #include <QString>
+qint64 getSizeOfFilesIn(QString path) {
+    QFileInfo fileInfo = QFileInfo(path);
+    if (fileInfo.isDir()) {
+        QDir dir = fileInfo.dir();
+        if (dir.cd(fileInfo.fileName())) {
+            qint64 currentDirectorySize = 0;
+            foreach (QFileInfo file, dir.entryInfoList(QDir::Files | QDir::NoDotAndDotDot, QDir::Size)) {
+                currentDirectorySize += file.size();
+            }
+            return currentDirectorySize;
+        }
+    }
+    else {
+        return fileInfo.size();
+    }
+}
+
 
 
 qint64 getSizeOf(QString path) {
