7f131b44602c02f73285d64f45f47bd51ccc2d1d
Stilb0n <stilbon01@gmail.com>
2024-10-10 00:33:36 +0700

    initial commit

---
+++ GUI/main.cpp
@@ -0,0 +1,22 @@
+#include "widget.h"
+#include <QApplication>
+#include "CalculationSize.h"
+#include <QDebug>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Widget w;
+    w.show();
+    return a.exec();
+}
+
+
+
+
+
+
+
+
+
